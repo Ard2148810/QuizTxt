2d9bf612cdf26bbc27368ea2b3e08ff63a0014a3
Adrian Ałaszewski <wns2148810@interia.pl>
2020-06-01 15:06:54 +0200

    Added and configured Maven

--- src/main/java/quiz/Controller.java
+++ src/main/java/quiz/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package quiz;
 
 public class Controller {
 }
--- src/main/java/quiz/QuizApp.java
+++ src/main/java/quiz/QuizApp.java
@@ -1,4 +1,4 @@
-package sample;
+package quiz;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class QuizApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FileSelectWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FileSelectWindow.fxml"));
         primaryStage.setTitle("Quiz");
         primaryStage.setMinHeight(200);
         primaryStage.setMinWidth(380);
